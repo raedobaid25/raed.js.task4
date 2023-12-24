73b0ae794566a2b46be09da4c58cabbd25ed2a09
raedobaid25 <147195118+raedobaid25@users.noreply.github.com>
2023-12-24 15:46:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector(".main").classList.add("demo");
+console.log(document.querySelector(".main"));
+
+document.querySelector(".test").classList.remove("vip");
+console.log(document.querySelector(".test"));
